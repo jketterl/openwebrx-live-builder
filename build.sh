e2d56441065c65e357293ed7c9ebe56857d82fbb
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-08-22 21:19:50 +0200

    use build cache

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 
 docker build --pull -t openwebrx-live-builder:latest .
-docker run -v$(pwd)/config:/live/config --privileged --name openwebrx-live-builder openwebrx-live-builder:latest
-docker cp openwebrx-live-builder:/live/live-image-amd64.hybrid.iso .
+
+docker volume create openwebrx-live-builder-cache
+
+RC=0
+docker run -v$(pwd)/config:/live/config -v openwebrx-live-builder-cache:/live/cache --privileged --name openwebrx-live-builder openwebrx-live-builder:latest &&\
+docker cp openwebrx-live-builder:/live/live-image-amd64.hybrid.iso .  || RC=$?
 docker rm openwebrx-live-builder
+exit $RC
