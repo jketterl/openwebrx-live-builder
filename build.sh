c52d316f7d34a12499f06e04f81e0eccdbef092f
Jakob Ketterl <jakob.ketterl@gmx.de>
2023-10-17 19:11:37 +0200

    use utc for timestamps

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ docker volume create openwebrx-live-builder-cache
 
 RC=0
 docker run -v openwebrx-live-builder-cache:/live/cache --privileged --name openwebrx-live-builder openwebrx-live-builder:latest &&\
-docker cp openwebrx-live-builder:/live/openwebrx-live-amd64.hybrid.iso $(date +"%Y-%m-%d")-openwebrx-live-amd64.iso  || RC=$?
+docker cp openwebrx-live-builder:/live/openwebrx-live-amd64.hybrid.iso $(date -u +"%Y-%m-%d")-openwebrx-live-amd64.iso  || RC=$?
 docker rm openwebrx-live-builder
 exit $RC
